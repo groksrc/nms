425440e21b00fd79c493cc8a669afbe1bc23361b
Drew Cain <drew@simpli.fi>
2015-05-20 10:39:31 -0500

    initial commit

---
+++ test/service.spec.js
@@ -0,0 +1,10 @@
+var expect = require("chai").expect;
+var service = require("../lib/service")();
+
+describe("service", function() {
+	describe("#start", function() {
+		it("has a start function", function() {
+			expect(service).to.have.ownProperty('start');
+		});
+	})
+})

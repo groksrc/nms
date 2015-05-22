bfd35c9806eba0811070ecfba493d6a25809241b
Drew Cain <drew@simpli.fi>
2015-05-22 09:18:16 -0500

    made the sample code a little clearer

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 // Only put the code necessary to start the service inside of service.js.  
-// Send any environment variables, command line parameters, etc into app.js as arguments to the constructor.
+// Send any environment variables, command line parameters, etc into app.js 
+// as arguments to the constructor.
 
 // TODO: Setup stuff here
 
-var service = require("./lib/app")(/* send args in here */);
+var app = require("./lib/app")(/* send args in here */);
 
-service.start();
+app.start();
 
+// Do whatever you want for stopping. Or leave it out entirely. 
+// This is for demonstration:
+app.stop();
--- test/app.spec.js
+++ test/app.spec.js
@@ -4,6 +4,9 @@ var service = require("../lib/app")();
 describe("app", function() {
 	describe("#start", function() {
 		
+		// You don't have to have a start and a stop function.
+		// Rip and replace, it's yours now.
+
 		it("has a start function", function() {
 			expect(service).to.have.ownProperty("start");
 		});
